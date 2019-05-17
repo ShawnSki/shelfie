f417962ec0c555226af24c3673277708b6db32b8
shawn's macbook <shawnscivally@shawn-ski.local>
2019-05-17 10:47:05 -0600

    database section completed

--- server/index.js
+++ server/index.js
@@ -1,13 +1,30 @@
+require('dotenv').config();
 const express = require('express');
-
+const massive = require('massive');
 const controller = require('./controller');
 
+// env variables
+const {
+    SERVER_PORT,
+    CONNECTION_STRING
+} = process.env;
 
+// server setup
 const app = express();
 app.use(express.json());
 
+// database connection
+massive(CONNECTION_STRING)
+    .then(dbInstance => {
+        app.set('db', dbInstance);
+        console.log('⛷ Freshies! Database connected.')
+    })
+    .catch(error => {
+        if (error) throw error;
+    });
 
-const PORT = 8888;
-app.listen(PORT, () => {
-    console.log(`🤙 Aloha and mahalo on port: ${PORT}`);
+
+// app is listening
+app.listen(SERVER_PORT, () => {
+    console.log(`🤙 Aloha and mahalo on port: ${SERVER_PORT}`);
 });
